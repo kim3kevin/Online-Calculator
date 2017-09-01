be6a50a063575c53fc78972e029f815cd2358ee3
Sang Hyun Kim <kim3kevin@gmail.com>
2017-09-01 15:21:41 +0900

    added more functionalities and style

--- script.js
+++ script.js
@@ -23,39 +23,39 @@ $(document).ready(function(){
 
 	$("#add").click(function(){
 		var addition = function(num1,num2){
-			return parseInt(num1) + parseInt(num2);
+			return parseFloat(num1) + parseFloat(num2);
 		}
 		firstnum = num;
 		num = "";
-		$("#calc_screen").text(0);
+		$("#calc_screen").text("");
 		operator = addition;
 	});
 	$("#subtract").click(function(){
 		var subtraction = function(num1,num2){
-			return parseInt(num1) - parseInt(num2);
+			return parseFloat(num1) - parseFloat(num2);
 		}
 		firstnum = num;
 		num = "";
-		$("#calc_screen").text(0);
+		$("#calc_screen").text("");
 		operator = subtraction;
 
 	});
 	$("#divide").click(function(){
 		var division = function(num1,num2){
-			return parseInt(num1)/parseInt(num2);	
+			return parseFloat(num1)/parseFloat(num2);	
 		}
 		firstnum = num;
 		num = "";
-		$("#calc_screen").text(0);
+		$("#calc_screen").text("");
 		operator = division;
 	});
 	$("#multiply").click(function(){
 		var multiplication = function(num1,num2){
-			return parseInt(num1) * parseInt(num2);
+			return parseFloat(num1) * parseFloat(num2);
 		}
 		firstnum = num;
 		num = "";
-		$("#calc_screen").text(0);
+		$("#calc_screen").text("");
 		operator = multiplication;
 	});
 	$("#equal").click(function(){
@@ -63,8 +63,52 @@ $(document).ready(function(){
 		$("#calc_screen").text(total);
 		operator = "";
 		firstnum = "";
+		num = String(total);
+		total = "";
+	});
+	$("#power").click(function(){
+		var exponent = function(num1,num2){
+		return Math.pow(parseFloat(num1),parseFloat(num2));
+		};
+		firstnum = num;
 		num = "";
-		})
+		$("#calc_screen").text("");
+		operator = exponent;
+	});
+
+	$("#modulus").click(function(){
+		var modulus = function(num1,num2){
+			return parseFloat(num1)%parseFloat(num2);
+		}
+		firstnum = num;
+		num = "";
+		$("#calc_screen").text("");
+		operator=modulus;
+	});
+
+	$("#root").click(function(){
+		var root = function(num1,num2){
+			return Math.pow(parseFloat(num1),1/parseFloat(num2));
+		}
+		firstnum = num;
+		num = "";
+		$("#calc_screen").text("");
+		operator=root;
+	});
+
+	$("#negative").click(function(){
+		num = num * -1;
+		$("#calc_screen").text(num);
+	});
+
+	$("#decimal").click(function(){
+		num = num.concat(".");
+		$("#calc_screen").text(num);
+	});
+
+
+	
+	
 });	
 
 
